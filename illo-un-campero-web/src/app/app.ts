b47e608c35d29dd704b428862b7cc66c3fdba8a3
Miguel <miguelmaciasdelpino@gmail.com>
2026-01-13 22:02:23 +0100

    feat Union y verificacion Firebase

--- illo-un-campero-web/src/app/app.ts
+++ illo-un-campero-web/src/app/app.ts
@@ -1,13 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
 
+
+//@Component({
+  //selector: 'app-root',
+  //imports: [RouterOutlet],
+  //templateUrl: './app.html',
+  //styleUrl: './app.css'
+//})
+//export class App {
+  //protected readonly title = signal('illo-un-campero-web');
+//}
+import { Component, inject } from '@angular/core';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [AsyncPipe, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('illo-un-campero-web');
+// CAMBIA ESTO: De AppComponent a App
+export class App { 
+  private firestore = inject(Firestore);
+  campero$: Observable<any>;
+
+  constructor() {
+    const ref = doc(this.firestore, 'carta/campero_pollo');
+    this.campero$ = docData(ref);
+  }
 }
