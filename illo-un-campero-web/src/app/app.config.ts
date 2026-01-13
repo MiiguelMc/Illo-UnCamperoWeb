ec45c983c62a80d8d5d7f893a2a3bf842afa84ed
Miguel <miguelmaciasdelpino@gmail.com>
2026-01-13 21:35:55 +0100

    Clean Start Project

--- illo-un-campero-web/src/app/app.config.ts
+++ illo-un-campero-web/src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "illo-uncampero", appId: "1:478864200524:web:7ac0cc4c432d085f9d35e9", storageBucket: "illo-uncampero.firebasestorage.app", apiKey: "AIzaSyCuFeD7nPAxbG2rVQVHp2tKGhyCQt2ZFlQ", authDomain: "illo-uncampero.firebaseapp.com", messagingSenderId: "478864200524" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
   ]
 };
