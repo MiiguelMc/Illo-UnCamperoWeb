2aa0e121cacdc7631c10600fc0f70b5a2c7cee4b
Miguel <miguelmaciasdelpino@gmail.com>
2026-05-12 20:23:12 +0200

    feat: mover config Firebase a environment y actualizar workflows de deploy
    - app.config.ts: Firebase se inicializa desde environment.firebase en vez de hardcoded
    - environment.ts y environment.prod.ts: nueva estructura con Firebase config y placeholders
    - deploy.yml y firebase-hosting-merge.yml: sed actualizado para los nuevos placeholders
    - Elimina src.zip del repositorio
    - Actualiza fechas del sitemap

--- illo-un-campero-web/src/app/app.config.ts
+++ illo-un-campero-web/src/app/app.config.ts
@@ -10,17 +10,8 @@ import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth, browserLocalPersistence } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 // import { getStorage, provideStorage } from '@angular/fire/storage'; // activar cuando se configure el storage
-
-// datos del proyecto en la consola de firebase (web)
-const firebaseConfig = {
-  apiKey: "AIzaSyCuFeD7nPAxbG2rVQVHp2tKGhyCQt2ZFlQ",
-  authDomain: "illo-uncampero.firebaseapp.com",
-  projectId: "illo-uncampero",
-  storageBucket: "illo-uncampero.firebasestorage.app",
-  messagingSenderId: "478864200524",
-  appId: "1:478864200524:web:7ac0cc4c432d085f9d35e9"
-};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -35,7 +26,7 @@ export const appConfig: ApplicationConfig = {
       })
     }),
 
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => {
       const auth = getAuth();
       auth.setPersistence(browserLocalPersistence);
